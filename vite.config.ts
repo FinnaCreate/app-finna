4284e502bc720a6ed4d0fff12725921a7a9edfd4
Rory Gonzalez-Brown <81010708+RorzGonzalez@users.noreply.github.com>
2022-07-08 13:32:37 +0100

    Add sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import laravel from 'vite-plugin-laravel'
 
 export default defineConfig({
-    plugins: [laravel({})]
+    plugins: [laravel({})],
+    css: { devSourcemap: true },
+    build: { sourcemap: true }
 })
